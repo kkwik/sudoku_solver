6262f21bb708f5b4fbd9f89fdb85df3f1d721d73
kkwik <kevinkwik6@gmail.com>
2025-01-12 11:44:25 -0800

    add board input check

--- main.c
+++ main.c
@@ -1,11 +1,20 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char **argv) {
+	
 	if (argc != 2) {
 		printf("Incorrect number of arguments\n");
 		return EXIT_FAILURE;
 	}
+
+	char* boardInput = *(argv + 1);
+
+	if (strlen(boardInput) != 81) {
+		printf("Incorrect board input size: 81 inputs needed, %lu provided\n", strlen(boardInput));
+	}
+
 
 }
