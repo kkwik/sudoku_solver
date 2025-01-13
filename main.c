53b8edcc97d1dc037b01078c4c78fbdfe2f57dbb
kkwik <kevinkwik6@gmail.com>
2025-01-12 17:20:23 -0800

    add user input, action switch, and move towards array syntax

--- main.c
+++ main.c
@@ -4,59 +4,74 @@
 #include <string.h>
 #include <stdbool.h>
 
-int* parseBoardString(int *board, char *boardStr) {
+void parseBoardString(int board[9][9], char *boardStr) {
 	for (int i = 0; i < 81; i++) {
-		*(board + i) = boardStr[i] - '0';
+		int r = i / 9;
+		int c = i % 9;
+
+		board[r][c] = boardStr[i] - '0';
 	}
-	return board;
 }
 
-int boardVal(int *board, int rowI, int colI) {
-	return *(board + (9 * rowI) + colI);
+char getInput() {
+	char input = fgetc(stdin);
+
+	// Clear stdin
+	char c;
+	while ((c = getchar()) != '\n' && c != EOF) { }
+
+	return input;
 }
 
-void printBoard(int *board) {
+void printBoard(int board[9][9]) {
 	printf("-------------------------\n");
-	for (int i = 0; i < 9; i++) {
-		if (i != 0 && i % 3 == 0) {
+	for (int r = 0; r < 9; r++) {
+		if (r != 0 && r % 3 == 0) {
 			printf("|-------+-------+-------|\n");
 		}
 
-		printf("| %d %d %d | %d %d %d | %d %d %d |\n", boardVal(board, i, 0), boardVal(board, i, 1), boardVal(board, i, 2), boardVal(board, i, 3), boardVal(board, i, 4), boardVal(board, i, 5), boardVal(board, i, 6), boardVal(board, i, 7), boardVal(board, i, 8));
+		for (int c = 0; c < 9; c++) {
+			if (c % 3 == 0) {
+				printf("| ");
+			}
+			printf("%d ", board[r][c]);
+
+			if (c == 8) {
+				printf("|\n");
+			}
+		}
 	}
 	printf("-------------------------\n");
 }
 
-bool rowHas(int *board, int rowI, int val) {
-	for (int colI = 0; colI < 9; colI++) {
-		if (boardVal(board, rowI, colI) == val) {
+bool rowHas(int board[9][9], int r, int val) {
+	for (int c = 0; c < 9; c++) {
+		if (board[r][c] == val) {
 			return true;
 		}
 	}
 	return false;
 }
 
-bool colHas(int *board, int colI, int val) {
-	for (int rowI = 0; rowI < 9; rowI++) {
-		if (boardVal(board, rowI, colI) == val) {
+bool colHas(int board[9][9], int c, int val) {
+	for (int r = 0; r < 9; r++) {
+		if (board[r][c] == val) {
 			return true;
 		}
 	}
 	return false;
 }
 
-bool sqrHas(int *board, int sqrI, int val) {
+bool sqrHas(int board[9][9], int sqrI, int val) {
 	int sqrRI, sqrCI;
 	sqrRI = sqrI / 3;
 	sqrCI = sqrI % 3;
 
 	for (int i = 0; i < 9; i++) {
-		int rowI, colI;
+		int r = (3 * sqrRI) + (i / 3);
+		int c = (3 * sqrCI) + (i % 3);
 
-		rowI = (3 * sqrRI) + (i / 3);
-		colI = (3 * sqrCI) + (i % 3);
-
-		if (boardVal(board, rowI, colI) == val) {
+		if (board[r][c] == val) {
 			return true;
 		}
 	}
@@ -76,8 +91,54 @@ int main(int argc, char **argv) {
 		printf("Incorrect board input size: 81 inputs needed, %lu provided\n", strlen(boardInput));
 	}
 
-	int *board = malloc(81 * sizeof(int));
+	int (*board)[9] = malloc(81 * sizeof(int));
 	parseBoardString(board, boardInput);
 
-	printBoard(board);
+	char input = ' ';
+	do {
+		input = ' ';
+
+		printf("\n");
+		printBoard(board);
+		printf("\n");
+		printf("Select Action: \n"
+			"\tq: Exit\n"
+			"\te: Edit\n"
+			"> "
+		);
+
+		input = getInput(); 
+
+		// Handle input
+		switch (input) {
+			case 'q':
+				free(board);
+				break;
+			case 'e':
+				printf("Row to modify: ");
+				int row = getInput() - '0';
+				if (row < 0 || row > 8) {
+					break;
+				}
+
+				printf("Col to modify: ");
+				int col = getInput() - '0';
+				if (col < 0 || col > 8) {
+					break;
+				}
+
+				printf("New value: ");
+				int newVal = getInput() - '0';
+				if (newVal < 0 || newVal > 8) {
+					break;
+				}
+
+				board[row][col] = newVal;
+				break;
+			default:
+				printf("Invalid option: %c\n", input);
+				break;
+		}
+
+	} while (input != 'q');
 }
